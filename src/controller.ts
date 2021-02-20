40825fb448eba46a9e35633194cd1900aec92e62
Moritz <moritz@bantleon.me>
2021-02-21 00:32:38 +0100

    Feature/client patch route (#1)
    * 🎁 created patch route for updating station status
    
    * 🎁 added patch route

--- src/controller.ts
+++ src/controller.ts
@@ -37,6 +37,35 @@ const getStation = async (req: any, res: any, next: Function) => {
 		.catch((err) => (res.status(500).json({ message: err.message })))
 }
 
+const setStationStatus = async (req: any, res: any) => {
+	const stationRef =  db.collection('station').doc(req.params.id)
+
+	try {
+		let station: any = undefined;
+		await stationRef.get()
+			.then((value) => {
+				if (!value){
+					return res.status(404).json({ message: 'Cant find station'})
+				}
+				station = value?.data()
+			})
+
+
+		//only change status if its not the same
+		if (req.query.fill !== station?.isFilled){
+			stationRef.update({
+				isFilled: req.query.fill,
+				filledLastTime:(req.query.fill? Date.now(): station.filledLastTime)
+			})
+			res.status(200).json({ message: 'Field has been toggled ', hasUpdated: true})
+		} else {
+			res.status(200).json({ message: 'Please use a different status', hasUpdated: false})
+		}
+	} catch (err) {
+		res.status(500).json({ message: err.message })
+	}
+}
+
 const whitelist = ['localhost:5000', 'grabyourshit.de']
 const corsOptions = {
   origin: function (origin: any, callback: any) {
@@ -48,4 +77,4 @@ const corsOptions = {
   }
 }
 
-export { getStations, getStation, corsOptions }
+export { getStations, getStation, corsOptions, setStationStatus }
--- src/routes/stations.ts
+++ src/routes/stations.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import { getStations, getStation } from '../controller'
+import { getStations, getStation, setStationStatus } from '../controller'
 
 
 const router = Router()
@@ -15,6 +15,13 @@ router.get('/:id', getStation, async (req: any, res: any) => {
 	res.json(res.station)
 })
 
+router.patch('/:id', async (req: any, res: any) => {
+	if (req.query.fill){
+		setStationStatus(req, res)
+	} else {
+		res.status(500).json({ message: 'add valid queryparams' })
+	}
+})
 
 module.exports = router
 
